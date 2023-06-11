4311a8521cfd5de9cacdae1edc598ba8bddb1c09
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-11 10:40:20 +0600

    routes and error page added

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        errorElement: <ErrorPage></ErrorPage>,
+        element: <Main></Main>,
+        children: [
+        ]
+    },
+]);
+export default router;
