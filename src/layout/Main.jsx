8ff23c868b3ed4d0f6d75584d568a8947e290d74
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-11 11:23:35 +0600

    footer added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,12 +2,14 @@
 
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../pages/Shared/NavigationBar/NavigationBar";
+import Footer from "../pages/Shared/Footer.jsx/Footer";
 
 const Main = () => {
     return (
         <div className="">
             <NavigationBar></NavigationBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
