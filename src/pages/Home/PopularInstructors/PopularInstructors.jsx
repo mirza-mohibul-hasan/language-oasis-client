7a2a93513fc63f4d4cdbcfea588b900821f70cbd
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-11 11:28:23 +0600

    Home Structure added

---
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const PopularInstructors = () => {
+    return (
+        <div>
+            Popular Instructors
+        </div>
+    );
+};
+
+export default PopularInstructors;
